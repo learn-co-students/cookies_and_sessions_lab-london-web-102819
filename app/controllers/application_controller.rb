05cf2fff487238a978472c626464d00450c9873d
github username <github email address>
2019-11-28 12:13:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def cart
+    session[:cart] ||= []
+  end
+
+  def home
+
+  end
+
+  def add_to_cart
+    cart << params[:product]
+    redirect_to products_path
+  end
+
+  
 end
