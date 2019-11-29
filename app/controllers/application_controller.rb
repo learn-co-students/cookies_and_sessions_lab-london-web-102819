f5b74afe6acd75c8c672eded48a3834d904b3730
PianoFF <Fangfei1star@gmail.com>
2019-11-29 14:50:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :cart
+  # helper_method :cart
+  ## if this def cart was put in app/helpers/application_helper.rb, then you'd need the helper_metho :cart 
+  ## in order for it to be accessible throughout all child controllers 
 
   def cart
     session[:cart] ||= []
